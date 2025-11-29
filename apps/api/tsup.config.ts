9e1c1e4fafbfdece62e43220a1f7784ae2b9dff4
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-11-29 13:09:04 -0300

    chore: configura arquivo tsup.config.ts

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src'],
-    outDir: './dist',
-    format: ['esm', 'cjs'],
-    target: 'node20',
+    // outDir: './dist',
+    // format: ['esm', 'cjs'],
+    // target: 'node20',
     splitting: false,
     sourcemap: true,
     clean: true,
-    dts: false,
-    shims: false,
+    // dts: false,
+    // shims: false,
     noExternal: ['saas/auth', 'saas/env']
 })
