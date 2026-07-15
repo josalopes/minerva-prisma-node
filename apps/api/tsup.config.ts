ac11ef2ab561e27335152f9a4e56a8a84536a5fc
Francisco Josafá <josafa.lopes@inventione.com.br>
2026-07-15 08:06:03 -0300

    fix: refatora arquivo tsup.config.ts

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src'],
-    // outDir: './dist',
-    // format: ['esm', 'cjs'],
-    // target: 'node20',
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    // dts: false,
-    // shims: false,
-    noExternal: ['saas/auth', 'saas/env']
+  entry: ['src'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  noExternal: [/^@saas\//],
 })
